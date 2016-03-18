027dcbd3d497c8f24785490a17360d0ee5b0b9a1
Evan Cameron <evan@vectorface.ca>
2016-03-18 16:15:42 -0400

    move Counter to containers

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
-import Counter from './Counter';
 import FooView from './FooView';
 import BarView from './BarView';
 import NotFoundView from './NotFoundView';
 import HomeView from './HomeView';
-export { Counter, FooView, BarView, NotFoundView, HomeView };
+export { FooView, BarView, NotFoundView, HomeView };
